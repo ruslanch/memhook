d36d4ad7605c8eeb095bdf57fdee51097992e860
Ruslan Chikirin <ruslanch@gmail.com>
2015-05-26 19:29:28 +0300

    New fixes

---
+++ src/memhook/staticbuf_alloc.cpp
@@ -0,0 +1,33 @@
+#include "staticbuf_alloc.hpp"
+#include <errno.h>
+
+namespace memhook {
+
+char        StaticBufAlloc::tmpbuf_[1024];
+std::size_t StaticBufAlloc::tmppos_ = 0;
+
+static void* StaticBufAlloc::malloc(size_t size) {
+    if (tmppos_ + size >= sizeof(tmpbuf_)) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    void *ptr = tmpbuf_ + tmppos_;
+    tmppos_ += size;
+    return ptr;
+}
+
+static void* StaticBufAlloc::calloc(size_t nmemb, size_t size) {
+    const size_t total_size = nmemb * size;
+    void *ptr = malloc(total_size);
+    if (ptr == NULL)
+        return ptr;
+    memset(ptr, 0, total_size);
+    return ptr;
+}
+
+static void StaticBufAlloc::free(void *ptr) {
+    // do nothing
+}
+
+} // namespace memhook
